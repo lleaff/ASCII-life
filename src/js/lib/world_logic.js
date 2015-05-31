3f6cb5bfb616a90c32d6e83e297effc82fe9a630
lleaff <lleaff@users.noreply.github.com>
2015-05-31 19:22:54 +0200

    >hasType(), add check that elements exist

--- src/js/lib/world_logic.js
+++ src/js/lib/world_logic.js
@@ -73,12 +73,12 @@ if (World.Element === undefined) World.Element = {};
 if (World.Elements === undefined) World.Elements = {};
 
 World.Element.hasType = function(element, elementType) {
-	return element.type.indexOf(elementType) !== -1;
+	return element && element.type.indexOf(elementType) !== -1;
 };
 
 /* Return the number of elements that have a type of elementType */
 World.Elements.hasType = function(elements, elementType) {
-	return elements.filter(function(el) {
+	return elements && elements.filter(function(el) {
 			return World.Element.hasType(el, elementType);
 		}).length;
 };
