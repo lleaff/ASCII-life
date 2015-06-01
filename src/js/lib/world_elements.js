6ecd127d7dafeb8c7c1d1a44c2b4bc597ecd7b03
lleaff <lleaff@users.noreply.github.com>
2015-06-01 18:45:16 +0200

    +Elem.nextTo() & +descriptive comments

--- src/js/lib/world_elements.js
+++ src/js/lib/world_elements.js
@@ -3,11 +3,31 @@
 if (Elem === undefined) var Elem = {};
 if (Elems === undefined) var Elems = {};
 
-Elem.nextTo = function() {
-	/* TODO */
+/* Returns vectors relative to 'view'.position to cells whose elements
+ *  satisfy the condition given in 'traits', or null if none does.
+ *  'range' (default: 1) is the distance to scan. */
+Elem.nextTo = function(view, traits, range) {
+	var vectors = [];
+	if (range === undefined) range = 1;
+	Direction.forEach(function(direction) {
+		for (var i = 0, vector; i < range; ++i) {
+			vector = view.position.plus(
+				direction.times(new Vector(range, range)));
+			if (Elem.match(view.world.grid.get(vector)), traits)
+				vectors.push(vector);
+		}
+	});
+	return vectors[0] === undefined ? null : vectors;
 };
 
-Elem.match = function(elements, traits) {
+/* Test the given array of elements for caracteristic given in 'traits'.
+ *  'traits' properties can be any supported element property as key
+ *  name, and the expected value as value.
+ *
+ * eg.: Avoid cells with solid animals:
+ *             !Elems.match(world.grid.get(vector),
+ *                           { types: ["animal"], solid: true }) */
+Elems.match = function(elements, traits) {
 	if (traits.types && !Elems.hasTypes(elements, traits.types)) {
 		return false; }
 	if (traits.solid && !Elems.isSolid(elements, traits.solid)) {
@@ -15,14 +35,13 @@ Elem.match = function(elements, traits) {
 	return true; /* If all conditions pass, return true */
 };
 
-
+/* =Test functions
+ * ------------------------------ */
 /* Return the number of elements that satisfy the given test function */
 Elems._test = function(callback, elements) {
 	return elements && elements.filter(callback(elem)).length;
 };
 
-/* =Test functions
- * ------------------------------ */
 
 /* types */
 Elem.hasType = function(elementType, elem) {
