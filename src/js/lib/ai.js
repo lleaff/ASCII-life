93576ff0411ec3c2945b833fe45e42e2a8fc07e6
lleaff <lleaff@users.noreply.github.com>
2015-06-01 14:39:26 +0200

    ~clean

--- src/js/lib/ai.js
+++ src/js/lib/ai.js
@@ -13,9 +13,8 @@ Ai.prototype.keepMoving = function() {
 	if (this.actor.dir) {
 		var longestMove = this.actor.view.look(this.actor.dir)
 												.possibleMoves().pop();
-		if (longestMove) {
+		if (longestMove)
 			return this.world.actions.move(this.actor, longestMove);
-		}
 	}
 	return null;
 };
@@ -31,9 +30,7 @@ Ai.prototype.moveToward = function(elementType, defaultDirection) {
 	var reachable = this.actor.view.reachable(elementType);
 	shuffleArray(reachable); /* Avoid predictable resolution */
 	var moveVec = World.View.closest(reachable) || defaultDirection;
-	var res = //DEBUG (return)
-		 this.world.actions.move(this.actor, moveVec);
-	//if (this.actor.name === "wallHugger") console.log("res",res); return res;//DEBUG
+	return this.world.actions.move(this.actor, moveVec);
 };
 
 Ai.prototype.moveAlong = function(elementType, defaultDirection) {
